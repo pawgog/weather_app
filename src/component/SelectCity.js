6f95ec8d2f003dae0e06f69d28ac589cb5271b55
pawgog <p.gogola@interia.pl>
2020-11-22 23:26:45 +0100

    Select city added style.

--- src/component/SelectCity.js
+++ src/component/SelectCity.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 function SelectCity({ selectCity }) {
   return (
-    <>
+    <div className="weather-app-select-city">
       <label htmlFor="city">Change city:</label>
       <select name="city" id="city" onChange={(e) => selectCity(e.target)}>
         <option value="London">London</option>
         <option value="Malmo">Malmo</option>
+        <option value="Barcelona">Barcelona</option>
         <option value="Seattle">Seattle</option>
       </select>
-    </>
+    </div>
   );
 }
 
